82a0599fdae8fc9338fdb80e317f3482654c077d
Alex <alex@colburn.org>
2014-11-10 16:27:42 -0800

    no message

--- mpir.rb
+++ mpir.rb
@@ -2,17 +2,13 @@ require 'formula'
 
 class Mpir < Formula
   homepage 'http://mpir.org/'
-  url 'http://mpir.org/mpir-2.6.0.tar.bz2'
-  sha1 '28a91eb4d2315a9a73dc39771acf2b99838b9d72'
+  url 'http://mpir.org/mpir-2.7.0-alpha12.tar.bz2'
+  sha1 '6a46071d007a5284dbb67c4db70306deeb3e6513'
+  version '2.7.0'
 
   option '32-bit'
   option 'skip-check', 'Do not run `make check` to verify libraries'
 
-  fails_with :clang do
-    cause <<-EOS.undent
-      Build system requires a GCC compatible compiler.
-      EOS
-  end
 
   def install
     if MacOS.prefer_64_bit? and not build.build_32_bit?
@@ -22,7 +18,8 @@ class Mpir < Formula
     end
 
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+            "--enable-cxx", "--enable-gmpcompat",
+            "--prefix=#{prefix}"
     system "make"
     system "make", "install"
     system "make", "check" unless build.include? "skip-check"
